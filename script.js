da06b26de15c9901776651463475137c0e3eddae
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-03-06 18:36:58 +0000

    Added variables

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ console.log("Welcome to JavaScript!");
 
 // console.log() is a FUNCTION that displays info
 console.log("My name is Simeon");
+
+//DECLARE  a new VARIABLE to hold some data
+let pokemon;
+
+// ASSIGN a VALUE to that variable
+//like putting an item in a box
+pokemon = "pikachu";
+
+// DECLARE + ASSIGN in one statement
+let luckyNumber = 25;
+
+//"PASS IN" a variable to a FUNCTION
+console.log(luckyNumber);
+
+// CHANGE the value of an existing variable
+let raining = false;
+console.log(raining);
+raining = true;
+console.log(raining);
+
+// COPY data from one variable into another
+let breakfast = "Seeded bread";
+let lunch;
+lunch = breakfast;
+// Both variables hold the same value
+console.log(breakfast);
+console.log(lunch);
